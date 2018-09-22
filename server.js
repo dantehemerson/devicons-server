57b8eeb58bbeeae6223a22fb33c30289fd9a0545
dantehemerson <dantehemerson@gmail.com>
2018-09-21 22:54:01 -0500

    Getting PORT from Heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http')
 const createApp = require('./app')
 
+const port = process.env.PORT || 5000
+
 const startServer = () =>{
 	const server = http.createServer(createApp())
-	server.listen(5000, console.log('Listening on http://localhost:5000'))
+	server.listen(port, console.log(`Listening on port ${port}, Ctrl+C to stop`))
 }
 
 startServer()
