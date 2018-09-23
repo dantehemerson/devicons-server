5c186b61ab0d71fb195c936ec61fbc422c103df4
dantehemerson <dantehemerson@gmail.com>
2018-09-23 01:23:53 -0500

    :hammer: Settings

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const createApp = () => {
 	const app = express()
+
+	app.disable('x-powered-by')
+	app.set('views', path.join(__dirname, 'views'))
+	app.set('view engine', 'pug')
+
 	app.get('/', (req, res) => {
 		res.send('Hello World')
 	})
